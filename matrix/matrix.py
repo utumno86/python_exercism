59718d80657647ca32ac73b5b7cf3685811c01a7
Michael Nash <michael@spatialnetworks.com>
2019-06-06 15:36:59 -0400

    first pass at python matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,16 @@
 class Matrix(object):
+
     def __init__(self, matrix_string):
-        pass
+        self.matrix = []
+        rows = matrix_string.split("\n")
+        for row in rows:
+            self.matrix.append(list(map(lambda element: int(element), row.split(' '))))
 
     def row(self, index):
-        pass
+        return self.matrix[index - 1]
 
     def column(self, index):
-        pass
+        from_columns = []
+        for row in self.matrix:
+            from_columns.append(row[index -1])
+        return from_columns
