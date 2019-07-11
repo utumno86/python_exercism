f6118bf585bcffdabe1a524f9374b37e283f733e
Michael Nash <michael@spatialnetworks.com>
2019-07-11 12:46:29 -0400

    added list comprehensions

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -2,15 +2,11 @@ class Matrix(object):
 
     def __init__(self, matrix_string):
         self.matrix = []
-        rows = matrix_string.split("\n")
-        for row in rows:
-            self.matrix.append(list(map(lambda element: int(element), row.split(' '))))
+        rows = matrix_string.splitlines()
+        self.matrix = ([[int(element) for element in row.split(' ')] for row in rows])
 
     def row(self, index):
         return self.matrix[index - 1]
 
     def column(self, index):
-        from_columns = []
-        for row in self.matrix:
-            from_columns.append(row[index -1])
-        return from_columns
+        return [row[index-1] for row in self.matrix]
