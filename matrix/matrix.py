15f3a3f5658c13cc67cb81bc0e7de33055f9a02f
Michael Nash <michael@spatialnetworks.com>
2019-07-12 12:23:23 -0400

    slight update to matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -6,7 +6,7 @@ class Matrix(object):
         self.matrix = ([[int(element) for element in row.split(' ')] for row in rows])
 
     def row(self, index):
-        return self.matrix[index - 1]
+        return self.matrix[index - 1][:]
 
     def column(self, index):
         return [row[index-1] for row in self.matrix]
