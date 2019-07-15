006fac7a59139f1b4593cc6f212ca7419ab6b6ef
Michael Nash <michael@spatialnetworks.com>
2019-07-15 14:42:13 -0400

    updated matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -2,7 +2,7 @@ class Matrix(object):
 
     def __init__(self, matrix_string):
         rows = matrix_string.splitlines()
-        self.matrix = ([[int(element) for element in row.split(' ')] for row in rows])
+        self.matrix = [[int(element) for element in row.split(' ')] for row in rows]
 
     def row(self, index):
         return self.matrix[index - 1][:]
