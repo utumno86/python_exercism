056008ad6c84188338bbf5ec7ec6d64a61c31b39
Michael Nash <michael@spatialnetworks.com>
2019-08-05 13:44:43 -0400

    changed my dict to a tuple of tuples

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,11 +1,6 @@
 def convert(number):
-
-    soundPairs = {
-      3: "Pling",
-      5: "Plang",
-      7: "Plong"
-    }
-    raindrops = [soundPairs[factor] for factor in soundPairs if number%factor == 0]
+    soundPairs = ((3, 'Pling'), (5, 'Plang'), (7, 'Plong'))
+    raindrops = [sound for factor, sound in soundPairs if number%factor == 0]
     if len(raindrops) == 0:
       return str(number)
     else:
