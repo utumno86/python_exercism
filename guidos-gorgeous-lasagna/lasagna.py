5cb3660fa4f49a8ebff0cbf8c13cd108bd0ad252
Michael Nash <minash@assurance.com>
2021-09-10 14:54:43 -0400

    annoying automated fixes

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -1,3 +1,5 @@
+"""Information about lasagna bake time"""
+
 EXPECTED_BAKE_TIME = 40
 
 
@@ -23,7 +25,7 @@ def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """
     Return elapsed cooking time.
 
-    This function takes two numbers representing the number of layers & the time already spent 
+    This function takes two numbers representing the number of layers & the time already spent
     baking and calculates the total elapsed minutes spent cooking the lasagna.
     """
     return elapsed_bake_time + preparation_time_in_minutes(number_of_layers)
