70400ca0db5dd650e6b744b004b1644e312f804e
Michael Nash <minash@assurance.com>
2021-09-14 10:13:04 -0400

    ghost gobble arcade game

---
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -0,0 +1,50 @@
+"""Code to determine pac man score"""
+
+
+def eat_ghost(power_pellet_active, touching_ghost):
+    """
+
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost:  bool - is the player touching a ghost?
+    :return: bool
+    """
+    if(power_pellet_active and touching_ghost):
+        return True
+    return False
+
+
+def score(touching_power_pellet, touching_dot):
+    """
+
+    :param touching_power_pellet: bool - does the player have an active power pellet?
+    :param touching_dot:  bool - is the player touching a dot?
+    :return: bool
+    """
+    if(touching_power_pellet or touching_dot):
+        return True
+    return False
+
+
+def lose(power_pellet_active, touching_ghost):
+    """
+
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost: bool - is the player touching a ghost?
+    :return: bool
+    """
+    if(touching_ghost and not power_pellet_active):
+        return True
+    return False
+
+
+def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
+    """
+
+    :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost:  bool - is the player touching a ghost?
+    :return: bool
+    """
+    if(has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)):
+        return True
+    return False
